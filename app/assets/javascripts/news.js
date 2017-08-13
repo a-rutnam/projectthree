f7d9017f1cb17e11ef7ebddedec4dd74d962d83f
a-rutnam <a.rutnam@gmail.com>
2017-08-13 13:57:21 +1000

    all boxes moving

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,47 +1,47 @@
-function myFunction() {
-
-document.getElementById('modalOpen').style.display = "none";
-
-var canvas = createCanvas(windowWidth, windowHeight);
-
-  // document.getElementById('modalOpen').style.display = "none";
-
-
-// var canvas = document.getElementById('defaultCanvas0');
-// var c = document.getElementById("defaultCanvas0");
-//   var ctx = c.getContext("2d");
-// debugger;
-
-colorMode(HSB, 360, 100, 100);
-// console.log(frameRate());
-
-// pg = createGraphics(100, 80);
-
-
-// oscy = new p5.TriOsc(); // set frequency and type
-// oscy.amp(.5);
-// oscy.pan(-1);
-// oscy.start();
+// function myFunction() {
 //
-// oscbut = new p5.TriOsc(); // set frequency and type
-// oscbut.amp(0);
-// oscbut.pan(1);
-// oscbut.start();
-// capture = createCapture(VIDEO);
-// capture.size(320, 240);
-// capture.hide();
+// document.getElementById('modalOpen').style.display = "none";
 //
-// var myCanvas = createCanvas(600, 400);
-
- // myCanvas.parent('myContainer');
- console.log('canvas resize');
- $('canvas').css({
-   height: '100% !important',
-   width:  '100% !important'
- });
-
-//  createCanvas(windowWidth, windowHeight, WEBGL);
-//  ortho(-width, width, height, -height/2, 0.1, 100);
-textSize(44);
-
-}
+// var canvas = createCanvas(windowWidth, windowHeight);
+//
+//   // document.getElementById('modalOpen').style.display = "none";
+//
+//
+// // var canvas = document.getElementById('defaultCanvas0');
+// // var c = document.getElementById("defaultCanvas0");
+// //   var ctx = c.getContext("2d");
+// // debugger;
+//
+// colorMode(HSB, 360, 100, 100);
+// // console.log(frameRate());
+//
+// // pg = createGraphics(100, 80);
+//
+//
+// // oscy = new p5.TriOsc(); // set frequency and type
+// // oscy.amp(.5);
+// // oscy.pan(-1);
+// // oscy.start();
+// //
+// // oscbut = new p5.TriOsc(); // set frequency and type
+// // oscbut.amp(0);
+// // oscbut.pan(1);
+// // oscbut.start();
+// // capture = createCapture(VIDEO);
+// // capture.size(320, 240);
+// // capture.hide();
+// //
+// // var myCanvas = createCanvas(600, 400);
+//
+//  // myCanvas.parent('myContainer');
+//  console.log('canvas resize');
+//  $('canvas').css({
+//    height: '100% !important',
+//    width:  '100% !important'
+//  });
+//
+// //  createCanvas(windowWidth, windowHeight, WEBGL);
+// //  ortho(-width, width, height, -height/2, 0.1, 100);
+// textSize(44);
+//
+// }
